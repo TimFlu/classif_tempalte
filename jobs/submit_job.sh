28ca8d59743f74a8c517957f2cb9ea55e8a6fd4f
Tim Fluehmann <tim@fluehmann.net>
2024-11-22 12:06:22 +0100

    increased job time from 10 to 36h

--- jobs/submit_job.sh
+++ jobs/submit_job.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name="t1dexi_patchtst"
-#SBATCH --time=10:00:00
+#SBATCH --time=36:00:00
 #SBATCH --mem-per-cpu=2G
 #SBATCH --cpus-per-task=16
 #SBATCH --output=/storage/homefs/tf24s166/code/performance_metrics_estim/logs/log.txt
